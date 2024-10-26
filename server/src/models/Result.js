3a0c61ff993af0e24d23fd25bdc6ffc47e3b0106
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-10-26 22:57:19 +0300

    feat: add result modal
    Took 14 minutes

---
+++ server/src/models/Result.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../db/sequelize');
+const { v4: uuidv4 } = require('uuid');
+
+const Result = sequelize.define(
+    'Result',
+{
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: uuidv4,
+            primaryKey: true,
+        },
+        userId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: 'User',
+                key: 'id'
+            }
+        }
+},
+{
+    tableName: 'Results',
+    timestamps: true
+});
+
+module.exports = Result;
