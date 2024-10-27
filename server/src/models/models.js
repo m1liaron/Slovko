3c2183f4179d65805e9b5108f4d5b679bb9f1519
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-10-27 15:07:56 +0200

    feat: Result - ResultMode links
    Took 6 minutes

--- server/src/models/models.js
+++ server/src/models/models.js
@@ -2,6 +2,7 @@ const Card = require('./Card');
 const Group = require('./Group');
 const User = require('./User');
 const Result = require('./Result');
+const ResultMode = require('./ResultMode');
 
 // User - Group
 User.hasMany(Group, { foreignKey: 'userId', as: 'groups' });
@@ -14,6 +15,10 @@ Card.belongsTo(Group, { foreignKey: 'groupId', as: 'group' });
 // User - Result
 User.hasMany(Result, { foreignKey: 'userId', as: 'results' });
 Result.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+// Result - ResultMode
+Result.hasMany(ResultMode, { foreignKey: 'userId', as: 'mode' });
+ResultMode.belongsTo(Result, { foreignKey: 'resultId', as: 'result' });
 
 module.exports = {
     Card,
