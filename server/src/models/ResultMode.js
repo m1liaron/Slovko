ee4410ded0bbaca311ea5d78c9e256ae23acb76b
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-10-27 14:40:25 +0200

    feat: ResultMode
    Took 15 minutes

---
+++ server/src/models/ResultMode.js
@@ -0,0 +1,28 @@
+const {sequelize} = require("../db/sequelize");
+const {DataTypes} = require("sequelize");
+const {v4: uuidv4} = require("uuid");
+const { Result } = require("./models");
+
+const ResultMode = sequelize.define(
+"ResultMode",
+{
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: uuidv4,
+            primaryKey: true,
+        },
+        resultId: {
+            type: DataTypes.UUID,
+            references: {
+                model: Result,
+                key: 'id'
+            }
+        }
+    },
+    {
+        tableName: 'ResultsMode',
+        timestamps: true
+    }
+)
+
+module.exports = ResultMode;
