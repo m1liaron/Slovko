695c3fca11a9d13e3c7fcce718efa7be3ee0003d
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-10-29 22:49:35 +0200

    feat: getResults thunk
    Took 2 minutes

--- frontend/src/redux/resultsSlice.js
+++ frontend/src/redux/resultsSlice.js
@@ -9,6 +9,14 @@ export const saveResults = createAsyncThunk(
     }
 )
 
+export const getResults = createAsyncThunk(
+    'user/login', async (id) => {
+        const axiosInstance = createAuthorizedInstance();
+        const response = await axiosInstance.get(`/results/${id}`);
+        return response.data
+    }
+)
+
 const initialState = {
     results: [],
     isLoading: false,
@@ -28,10 +36,23 @@ const resultSlice = createSlice({
             })
             .addCase(saveResults.fulfilled, (state, action) => {
                 state.status = 'success';
-                state.results = action.payload;
+                state.results = [ state.results, ...action.payload ];
                 state.isLoading = false;
             })
             .addCase(saveResults.rejected, (state, action) => {
+                state.status = 'error';
+                state.isLoading = false;
+            })
+            .addCase(getResults.pending, (state, action) => {
+                state.status = 'loading';
+                state.isLoading = true;
+            })
+            .addCase(getResults.fulfilled, (state, action) => {
+                state.status = 'success';
+                state.results = action.payload;
+                state.isLoading = false;
+            })
+            .addCase(getResults.rejected, (state, action) => {
                 state.status = 'error';
                 state.isLoading = false;
             })
