9eaa6cb2b1557329fc89d7681876da74dfff63a9
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-10-29 17:30:09 +0200

    feat: saveResults controller
    Took 20 minutes

--- server/src/controllers/resultsController.js
+++ server/src/controllers/resultsController.js
@@ -1,16 +1,44 @@
 const { Result, ResultMode, WordResult } = require('../models/models');
 
-const saveResults = (req, res) => {
+const saveResults = async (req, res) => {
     const {
         body: {
             title,
-            flashCards
+            data,
         },
         user: { id }
     } = req;
     try {
+        const newResult = await Result.create({ title, userId: id });
+        const resultMode = await Promise.all(Object.keys(data).map(item => {
+            return ResultMode.create({
+                mode: item,
+                resultId: newResult.id
+            });
+        }));
+        await Promise.all(Object.values(data).map(item => {
+            return ResultMode.create({
+                word: item.word,
+                translate: item.translateWord,
+                isCorrect: item.isCorrect,
+                resultModeId: resultMode.id
+            });
+        }));
 
-        
+        const result = await Result.findOne({
+            where: { userId: id, id: newResult.id},
+            include: {
+                model: ResultMode,
+                include: [
+                    {
+                        model: WordResult,
+                        attributes: [ 'word', 'translate', 'isCorrect' ]
+                    }
+                ]
+            }
+        })
+
+        res.status(200).json(result);
     } catch (error) {
         res.status(400).send({ error: true, message: error.message || 'Error save results'});
     }
