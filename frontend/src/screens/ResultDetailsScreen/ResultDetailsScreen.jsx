d9ccf16245899caecd788469e0b45042dcd361dc
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-10-30 18:49:02 +0200

    feat: get result details
    Took 9 minutes

---
+++ frontend/src/screens/ResultDetailsScreen/ResultDetailsScreen.jsx
@@ -0,0 +1,38 @@
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native'
+import {SafeAreaView} from "react-native-safe-area-context";
+import styles from './ResultDetailsScreen.styles';
+import PressableButton from "../../common/components/PressableButton/PressableButton";
+import {useDispatch, useSelector} from "react-redux";
+import {getResultDetails, selectResult} from "../../redux/resultsSlice";
+
+const ResultDetailsScreen = ({ route }) => {
+    const { resultId } = route.params
+    const { results } = useSelector(selectResult);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getResultDetails(resultId));
+    });
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={styles.header}></View>
+            <View style={styles.buttonsContainer}>
+                <PressableButton text="Картки"/>
+                <PressableButton text="Вікторина"/>
+                <PressableButton text="Вгадай слово"/>
+            </View>
+            <View>
+                <FlatList
+                    data={}
+                    renderItem={({ item }) => (
+                        <View style={[styles.resultContainer, { backgroundColor: item.isCorrect ? "41ff12" : "f50000"}]}></View>
+                    )}
+                />
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default ResultDetailsScreen;
---
+++ frontend/src/screens/ResultDetailsScreen/ResultDetailsScreen.styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        backgroundColor: '#cdcdcd',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 20
+    },
+    resultContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+
+export default styles
