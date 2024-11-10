22883bd9b99c7b6e37eaa71564814355e54fdb19
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-11-10 21:46:42 +0200

    feat: function getSharedGroup
    Took 45 seconds

--- server/src/controllers/sharedGroupController.js
+++ server/src/controllers/sharedGroupController.js
@@ -29,7 +29,8 @@ const getAllSharedGroup = async (req, res) => {
 }
 const getSharedGroup = async (req, res) => {
     try {
-
+        const sharedGroup = await SharedGroup.findOne({ where: { id: req.params.id }});
+        res.status(200).json(sharedGroup);
     } catch(error) {
         res.status(500).json({ error: true, message: error.message || 'Server Error. Try again later.'})
     }
