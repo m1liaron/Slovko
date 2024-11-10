b7cd8482b12d23515d1580e95d2710e315749c40
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-11-10 21:35:07 +0200

    feat: sharedGroup: controller, route
    Took 4 minutes

---
+++ server/src/routes/sharedGroupRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const {  } = require('../controllers/sharedGroupController');
+
+router.get('/').post('/')
+router.get('/:id').post('/:id')
+
+module.exports = router;
