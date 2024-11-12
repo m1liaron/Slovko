65371e312eb05d9a7f2bc77af139bca1350017b1
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-11-12 18:43:33 +0200

    f
    Took 2 minutes

---
+++ frontend/src/redux/sharedGroup.js
@@ -0,0 +1,26 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {createAuthorizedInstance} from "../utils/createAuthorizedInstance";
+
+
+export const getAllGroups = createAsyncThunk(
+    'group/getAll', async (data) => {
+        const axiosInstance = await createAuthorizedInstance();
+        const response = await axiosInstance.get('/groups', data);
+        return response.data
+    }
+)
+
+
+const sharedGroupSlice = createSlice({
+    name:'sharedGroup',
+    initialState: {
+        sharedGroups: [],
+        status:'idle',
+        error: null
+    },
+    reducers:{},
+    extraReducers: (builder) => {}
+})
+
+export const selectGroup= (state) => state.sharedGroups.sharedGroups;
+export const groupReducers = sharedGroupSlice.reducer;
