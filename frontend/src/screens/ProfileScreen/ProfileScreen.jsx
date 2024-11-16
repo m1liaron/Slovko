af7fe2c031c7562e13557246eb634a0b57a05f5f
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-11-16 14:15:53 +0200

    feat: theme styles for profile screen anme
    Took 13 minutes

--- frontend/src/screens/ProfileScreen/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen/ProfileScreen.jsx
@@ -4,7 +4,6 @@ import {
     View,
     Text,
     TextInput,
-    StyleSheet,
     Pressable,
     Platform,
     Alert,
@@ -19,10 +18,12 @@ import * as ImagePicker from 'expo-image-picker';
 import {useAppTheme} from "../../contexts/ThemeProvider";
 import {Switch} from "react-native-gesture-handler";
 import {Feather} from "@expo/vector-icons";
+import styles from './ProfileScreen.styles';
 
 export default function ProfileScreen() {
     const { user } = useSelector(selectUser);
     const { theme, toggleTheme } = useAppTheme();
+    const colors = theme.colors;
     const navigation = useNavigation();
     const dispatch = useDispatch();
     const [image, setImage] = useState('');
@@ -132,51 +133,47 @@ export default function ProfileScreen() {
     }
 
     return (
-        <SafeAreaView style={[styles.container, { backgroundColor: theme.colors.background }]}>
-            <Text style={[styles.title, { color: theme.colors.primary }]}>Ваш профіль</Text>
+        <SafeAreaView style={[styles.container, { backgroundColor: colors.background }]}>
+            <Text style={[styles.title, { color: colors.primary }]}>Ваш профіль</Text>
 
                 <View style={styles.container}>
                     {user ? (
                         <View>
-                            {!isEditing ? (
-                                    <Text style={[styles.title, { color: theme.colors.primary }]}>{user.name}</Text>
-                            ) : null}
+                            {!isEditing && <Text style={[styles.title, { color: colors.primary }]}>{user.name}</Text> }
                             <Pressable onPress={onEditInfo}>
                                 <Text style={styles.editTitle}>Редагувати</Text>
                             </Pressable>
-                            {!isEditing ? (
-                                <Text style={[styles.textInfo, { color: theme.colors.primary } ]}>Особиста інформація</Text>
-                            ) : null}
-                            {isEditing ? (
+                            {!isEditing && <Text style={[styles.textInfo, { color: colors.primary } ]}>Особиста інформація</Text> }
+                            {isEditing && (
                                 <View>
                                     <Text style={styles.keyName}>Ім'я</Text>
-                                    <View style={styles.editInputContainer}>
+                                    <View style={[styles.editInputContainer, { backgroundColor: colors.lightBackground}]}>
                                         <MaterialIcons
                                             name="supervised-user-circle"
                                             size={35}
-                                            color="#000"
+                                            color={colors.iconColor}
                                         />
                                         <TextInput
-                                            style={styles.textInputStyle}
+                                            style={[styles.textInputStyle, { textDecorationStyle: colors.primary, color: colors.primary }]}
                                             value={userName}
                                             onChangeText={(text) => setUserName(text)}
                                         />
                                     </View>
                                 </View>
-                            ) : null}
+                            )}
 
                             {!isEditing ? (
-                                <View style={styles.infoList}>
-                                    <View style={styles.infoItem}>
+                                <>
+                                    <View style={[styles.infoItem, { backgroundColor: colors.lightBackground }]}>
                                         <View style={styles.flex}>
-                                            <MaterialIcons name="email" size={35} color="#000" />
-                                            <Text style={styles.keyName}>Пошта</Text>
+                                            <MaterialIcons name="email" size={35} color={colors.iconColor} />
+                                            <Text style={[styles.keyName, { color: colors.primary}]}>Пошта</Text>
                                         </View>
-                                        <Text style={styles.userInfoText}>{user.email}</Text>
+                                        <Text style={[styles.userInfoText, { color: colors.primary}]}>{user.email}</Text>
                                     </View>
-                                </View>
+                                </>
                             ) : (
-                                <View>
+                                <>
                                     <Text style={styles.keyName}>Пошта</Text>
                                     <View style={styles.editInputContainer}>
                                         <MaterialIcons name="email" size={35} color="#000" />
@@ -186,12 +183,12 @@ export default function ProfileScreen() {
                                             onChangeText={(text) => setUserEmail(text)}
                                         />
                                     </View>
-                                </View>
+                                </>
                             )}
 
                             {!isEditing ? (
                                 <View style={styles.infoList}>
-                                    <Text style={[styles.textInfo, { color: theme.colors.primary }]}>Взаємодія</Text>
+                                    <Text style={[styles.textInfo, { color: colors.primary }]}>Взаємодія</Text>
                                     <Pressable
                                         style={[styles.infoItem, { backgroundColor: '#dcdcdc' }]}
                                         onPress={handleLogout}
@@ -247,91 +244,12 @@ export default function ProfileScreen() {
                                 </View>
                             )}
                         </View>
-                    ) : null}
+                    ) : (
+                        <View>
+                            <Text style={{ color: colors.primary }}>Немає інформації про данного користувача, перезайдіть у застосунок або в акаунт.</Text>
+                        </View>
+                    )}
                 </View>
         </SafeAreaView>
     );
 }
-
-const styles = StyleSheet.create({
-    avatarPhoto: {
-        borderRadius: 100,
-        height: 200,
-        marginRight: 8,
-        width: 200,
-    },
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        padding: 10,
-    },
-    editInputContainer: {
-        alignItems: 'center',
-        backgroundColor: '#ebebeb',
-        borderRadius: 20,
-        flexDirection: 'row',
-        gap: 10,
-        padding: 15,
-    },
-    editTitle: {
-        color: '#828282',
-        fontSize: 16,
-        fontWeight: 'bold',
-        textAlign: 'right',
-    },
-    flex: {
-        alignItems: 'center',
-        flexDirection: 'row',
-        gap: 10,
-    },
-    infoItem: {
-        marginTop: 10,
-        alignItems: 'center',
-        backgroundColor: '#ebebeb',
-        borderRadius: 20,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 15,
-    },
-    infoList: {},
-    keyName: {
-        color: '#828282',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    saveButton: {
-        backgroundColor: '#d2d2d2',
-        borderRadius: 10,
-        padding: 10,
-        textAlign: 'center',
-    },
-    textContainer: {},
-    textInfo: {
-        alignItems: 'flex-start',
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginVertical: 15,
-        textAlign: 'left',
-    },
-    textInputStyle: {
-        textDecorationColor: '#000',
-        textDecorationLine: 'underline',
-        textDecorationStyle: 'solid',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginTop: 16,
-        textAlign: 'center',
-    },
-    userInfo: {
-        alignItems: 'center',
-    },
-    userInfoContainer: {
-        flexDirection: 'row',
-        justifyContent: 'flex-end', // Align to the right
-    },
-    userInfoText: {
-        fontSize: 17,
-    },
-});
