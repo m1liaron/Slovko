bb39504532f8737ce642f3b2390b0da948aec5a1
Artiquee <amex7708@gmail.com>
2024-11-29 22:20:20 +0200

    feat(resultsController): return words length with statistics

--- server/src/controllers/resultsController.js
+++ server/src/controllers/resultsController.js
@@ -59,16 +59,18 @@ const getResultsStatistics = async (req, res) => {
                 if (index !== -1) {
                     result.mode.forEach(modeItem => {
                         if (!modeMonthSum[modeItem.mode]) {
-                            modeMonthSum[modeItem.mode] = new Array(resultsMonths.length).fill(0);
+                            modeMonthSum[modeItem.mode] = new Array(resultsMonths.length).fill(null).map(() => ({ mistakes: 0, wordLength: 0}));
                         }
 
                         const mistakesForCard = modeItem.words.reduce((acc, curr) => acc + curr.mistakesAmount, 0);
-                        modeMonthSum[modeItem.mode][index] += mistakesForCard;
+                        const wordCountForCard = modeItem.words.length;
+
+                        modeMonthSum[modeItem.mode][index].mistakes += mistakesForCard;
+                        modeMonthSum[modeItem.mode][index].wordLength += wordCountForCard;
                     });
                 }
             });
 
-            // Convert modeMonthSum object into an array format (if required)
             return modeMonthSum;
         }
 
