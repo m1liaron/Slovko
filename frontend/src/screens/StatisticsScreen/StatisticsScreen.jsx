34760d6e47da7accd98f9299e8fed7664040b849
Artiquee <amex7708@gmail.com>
2024-11-30 20:29:10 +0200

    feat: make funcitonality to change graphs

--- frontend/src/screens/StatisticsScreen/StatisticsScreen.jsx
+++ frontend/src/screens/StatisticsScreen/StatisticsScreen.jsx
@@ -22,6 +22,7 @@ const StatisticsScreen = () => {
     const dispatch = useDispatch();
     const [selectedMode, setSelectedMode] = useState('flashCards');
     const [selectedWordsMode, setSelectedWordsMode] = useState('wordLength'); // Mistakes || wordLength;
+    const [selectedGraph, setSelectedGraph] = useState('LineChart');
 
     useEffect(() => {
         dispatch(getResultsStatistics());
@@ -35,6 +36,79 @@ const StatisticsScreen = () => {
     const wordsModeOptions = [
         { label: 'Кількість слів', value: 'wordLength' },
     ];
+
+    const graphOptions = [
+        { label: 'Лінійний', value: 'LinearChart' },
+        { label: 'Стовпчатий', value: 'BarChart' },
+        { label: 'Пиріг', value: 'PieChart' },
+    ]
+
+    const renderGraph = () => {
+        const chartConfig = {
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 2,
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+                borderRadius: 16,
+            },
+        };
+
+        const data = {
+            labels: statistics.resultsMonths,
+            datasets: [
+                {
+                    data: statistics.amountMistakesCards[selectedMode][selectedWordsMode],
+                },
+            ],
+        };
+
+        switch (selectedGraph) {
+            case 'BarChart':
+                return (
+                    <BarChart
+                        data={data}
+                        width={Dimensions.get("window").width - 20}
+                        height={220}
+                        chartConfig={chartConfig}
+                        style={{ marginVertical: 8, borderRadius: 16 }}
+                    />
+                );
+            case 'PieChart':
+                return (
+                    <PieChart
+                        data={statistics.amountMistakesCards[selectedMode][selectedWordsMode].map((value, index) => ({
+                            name: statistics.resultsMonths[index],
+                            population: value,
+                            color: `rgba(131, 167, 234, ${1 - index * 0.1})`,
+                            legendFontColor: "#7F7F7F",
+                            legendFontSize: 15,
+                        }))}
+                        width={Dimensions.get("window").width - 20}
+                        height={220}
+                        chartConfig={chartConfig}
+                        accessor="population"
+                        backgroundColor="transparent"
+                        paddingLeft="15"
+                        absolute
+                        style={{ marginVertical: 8, borderRadius: 16 }}
+                    />
+                );
+            default:
+                return (
+                    <LineChart
+                        data={data}
+                        width={Dimensions.get("window").width - 20}
+                        height={220}
+                        chartConfig={chartConfig}
+                        bezier
+                        style={{ marginVertical: 8, borderRadius: 16 }}
+                    />
+                );
+        }
+    };
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
@@ -99,8 +173,8 @@ const StatisticsScreen = () => {
                 />
             </View>
 
-            <View>
-            <RNPickerSelect
+            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: 10 }}>
+                <RNPickerSelect
                         onValueChange={(value) => setSelectedGraph(value)}
                         items={graphOptions}
                         value={selectedGraph}
@@ -125,60 +199,10 @@ const StatisticsScreen = () => {
                             }
                         }}
                 />
+            </View>
 
-                {(statistics && statistics.amountMistakesCards) && (
-                    <LineChart
-                        data={{
-                            labels: statistics.resultsMonths,
-                            datasets: [
-                                {
-                                    data: statistics.amountMistakesCards[selectedMode][selectedWordsMode]
-                                }
-                            ]
-                        }}
-                        width={Dimensions.get("window").width - 200 } // from react-native
-                        height={220}
-                        yAxisLabel=""
-                        yAxisSuffix=""
-                        yAxisInterval={1} // optional, defaults to 1
-                        chartConfig={{
-                            backgroundColor: "#e26a00",
-                            backgroundGradientFrom: "#fb8c00",
-                            backgroundGradientTo: "#ffa726",
-                            decimalPlaces: 2, // optional, defaults to 2dp
-                            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                            style: {
-                                borderRadius: 16,
-                            },
-                            propsForDots: {
-                                r: "6",
-                                strokeWidth: "2",
-                                stroke: "#ffa726"
-                            },
-                        }}
-                        renderDotContent={({ x, y, index }) => (
-                            <Text
-                                key={index}
-                                style={{
-                                    position: "absolute",
-                                    top: y - 20, // Position above the circle
-                                    left: x - 10, // Center horizontally
-                                    fontSize: 10,
-                                    color: "#000",
-                                    fontWeight: "bold"
-                                }}
-                            >
-                                {statistics.amountMistakesCards.flashCards[index]}
-                            </Text>
-                        )}
-                        bezier
-                        style={{
-                            marginVertical: 8,
-                            borderRadius: 16
-                        }}
-                    />
-                )}
+            <View>
+                {statistics && statistics.amountMistakesCards && renderGraph()}
             </View>
         </SafeAreaView>
     );
