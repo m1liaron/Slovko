fc44bb485ba5d113f82d00af9a41b82f8453d9f4
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-12-01 18:43:09 +0200

    feat: filter reducer
    Took 15 minutes

--- frontend/src/redux/sharedGroupReducer/sharedGroupSlice.js
+++ frontend/src/redux/sharedGroupReducer/sharedGroupSlice.js
@@ -5,11 +5,19 @@ const sharedGroupSlice = createSlice({
     name:'sharedGroup',
     initialState: {
         sharedGroups: [],
+        filteredGroups: [],
         sharedGroup: {},
         status:'idle',
         error: null
     },
-    reducers:{},
+    reducers:{
+        filterResults: (state, action) => {
+            state.sharedGroups = state.filteredGroups.filter(item => item.title.startsWith(action.payload));
+        },
+        resetResults: (state) => {
+            state.sharedGroups = [...state.filteredGroups];
+        }
+    },
     extraReducers: (builder) => {
         builder
             .addCase(getAllSharedGroups.pending, state => {
@@ -18,6 +26,7 @@ const sharedGroupSlice = createSlice({
             .addCase(getAllSharedGroups.fulfilled, (state, action) => {
                 state.status = 'success';
                 state.sharedGroups = action.payload;
+                state.filteredGroups = action.payload;
             })
             .addCase(getAllSharedGroups.rejected, state => {
                 state.status = 'error';
@@ -29,6 +38,7 @@ const sharedGroupSlice = createSlice({
             .addCase(saveSharedGroup.fulfilled, (state, action) => {
                 state.status = 'success';
                 state.sharedGroups.push(action.payload);
+                state.filteredGroups.push(action.payload);
             })
             .addCase(saveSharedGroup.rejected, state => {
                 state.status = 'error';
