75731739be33fa9cc6704c460a356747e880a76f
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-12-24 18:25:47 +0200

    fix: path to patch
    Took 19 seconds

--- server/src/routes/groupRoute.js
+++ server/src/routes/groupRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getAllGroups, addGroup, removeGroup, getGroup, updateGroup} = require('../controllers/groupController');
 
 router.route('/').get(getAllGroups).post(addGroup);
-router.route('/:id').delete(removeGroup).get(getGroup).path(updateGroup);
+router.route('/:id').delete(removeGroup).get(getGroup).patch(updateGroup);
 
 module.exports = router;
