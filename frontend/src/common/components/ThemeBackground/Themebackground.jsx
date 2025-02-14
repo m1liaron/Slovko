f0544cad123e605dc0a778602cd1d3b390a4ba33
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-02-14 22:46:58 +0200

    feat: common background
    Took 13 minutes

---
+++ frontend/src/common/components/ThemeBackground/Themebackground.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useAppTheme } from '../../../contexts/ThemeProvider';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const ThemeBackground = ({ children }) => {
+  const {theme: { colors }} = useAppTheme();
+  return <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>{children}</SafeAreaView>;
+};
+
+export default ThemeBackground;
