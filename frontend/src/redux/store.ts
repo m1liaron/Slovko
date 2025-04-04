bbe5dfc5523206b03aa2f93fea4e74b956c94dd1
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-04-04 17:29:52 +0300

    feat: rewrite store.ts to use types

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -14,3 +14,7 @@ export const store = configureStore({
 		results: resultReducers,
 	},
 });
+
+export type AppStore = typeof store;
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch'];
