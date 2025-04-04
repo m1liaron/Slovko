9f9618f87cb343f48080f2f904e5e3b3efa0eed2
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-04-04 18:06:11 +0300

    refactore: types for userThunk.ts

--- frontend/src/redux/userReducer/userThunk.ts
+++ frontend/src/redux/userReducer/userThunk.ts
@@ -2,36 +2,37 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createAuthorizedInstance } from "../../utils/createAuthorizedInstance";
-const url = "http://192.168.31.196:3000";
+import { IUser } from "@/common/enums/types/user.type";
+const url: string = "http://192.168.31.196:3000";
 
-export const login = createAsyncThunk("user/login", async (data) => {
+const login = createAsyncThunk("user/login", async (data) => {
 	const response = await axios.post(`${url}/users/login`, data);
 	await AsyncStorage.setItem("token", response.data.token);
 	return response.data;
 });
 
-export const register = createAsyncThunk("user/register", async (data) => {
+const register = createAsyncThunk("user/register", async (data) => {
 	const response = await axios.post(`${url}/users/register`, data);
 	await AsyncStorage.setItem("token", response.data.token);
 	return response.data;
 });
 
-export const getUser = createAsyncThunk("user/get", async () => {
+const getUser = createAsyncThunk("user/get", async () => {
 	const axiosInstance = await createAuthorizedInstance();
 	const response = await axiosInstance.get("/users");
 	return response.data.user;
 });
 
-export const updateUser = createAsyncThunk(
+const updateUser = createAsyncThunk(
 	"user/update",
-	async ({ id, data }) => {
+	async ({ id, data }: {id: string, data: IUser}) => {
 		const axiosInstance = await createAuthorizedInstance();
 		const response = await axiosInstance.put(`/users/${id}`, data);
 		return response.data;
 	},
 );
 
-export const updateUserStreak = createAsyncThunk(
+const updateUserStreak = createAsyncThunk(
 	"user/updateUserStreak",
 	async () => {
 		const axiosInstance = await createAuthorizedInstance();
@@ -40,18 +41,18 @@ export const updateUserStreak = createAsyncThunk(
 	},
 );
 
-export const buyFreeze = createAsyncThunk(
+const buyFreeze = createAsyncThunk(
 	"user/buyFreeze",
-	async ({ froze }) => {
+	async ({ froze }: { froze: boolean}) => {
 		const axiosInstance = await createAuthorizedInstance();
 		const response = await axiosInstance.put("/users/streak/froze", { froze });
 		return response.data;
 	},
 );
 
-export const getUserStreakDates = createAsyncThunk(
+const getUserStreakDates = createAsyncThunk(
 	"user/getStreakDates",
-	async ({ month, year }) => {
+	async ({ month, year }: { month: Date; year: Date}) => {
 		const axiosInstance = await createAuthorizedInstance();
 		const response = await axiosInstance.get(
 			`/users/streak?month=${month}&year=${year}`,
@@ -59,3 +60,13 @@ export const getUserStreakDates = createAsyncThunk(
 		return response.data;
 	},
 );
+
+export {
+	login,
+	register,
+	getUser,
+	updateUser,
+	updateUserStreak,
+	buyFreeze,
+	getUserStreakDates,
+}
