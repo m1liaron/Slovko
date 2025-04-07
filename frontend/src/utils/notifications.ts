533334cc535cf8773351cc69bc780dbd261ee5c6
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-04-07 12:06:01 +0300

    fix: remove expo-permissions

--- frontend/src/utils/notifications.ts
+++ frontend/src/utils/notifications.ts
@@ -1,36 +1,33 @@
 import * as Notifications from "expo-notifications";
-import * as Permissions from "expo-permissions";
 
 Notifications.setNotificationHandler({
-	handleNotification: async () => ({
-		shouldShowAlert: true,
-		shouldPlaySound: false,
-		shouldSetBadge: false,
-	}),
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
 });
 
 export const requestNotificationPermission = async () => {
-	const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-	if (status !== "granted") {
-		const { status: newStatus } = await Permissions.askAsync(
-			Permissions.NOTIFICATIONS,
-		);
-		return newStatus === "granted";
-	}
-	return true;
+  const { status } = await Notifications.getPermissionsAsync();
+  if (status !== "granted") {
+    const { status: newStatus } = await Notifications.requestPermissionsAsync();
+    return newStatus === "granted";
+  }
+  return true;
 };
 
 export const scheduleNotification = async (
-	title: string,
-	body: string,
-	trigger: any,
+  title: string,
+  body: string,
+  trigger: any
 ) => {
-	await Notifications.scheduleNotificationAsync({
-		content: {
-			title,
-			body,
-			sound: true,
-		},
-		trigger,
-	});
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title,
+      body,
+      sound: true,
+    },
+    trigger,
+  });
 };
