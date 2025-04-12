447efe278f7f75fb60eb3871150ff989645fa7ca
Artiquee <amex7708@gmail.com>
2025-04-12 14:53:39 +0300

    feat: add replace

--- frontend/src/screens/ResultsScreen/ResultsScreen.tsx
+++ frontend/src/screens/ResultsScreen/ResultsScreen.tsx
@@ -60,13 +60,13 @@ const ResultsScreen = () => {
 
 	useEffect(() => {
 		setPage(1);
-		dispatch(getResults({ year: showResultsYear, month: showResultsMonth, page: 1, replace: true }));
+		dispatch(getResults({ year: showResultsYear, month: showResultsMonth + 1, page: 1, replace: true }));
 	}, [dispatch, showResultsMonth, showResultsYear]);
 
 	const handleLoadMore = () => {
 		if(haveMoreResults && !isLoading) {
 			const nextPage = page + 1;
-			dispatch(getResults({ year: showResultsYear, month: showResultsMonth, page: nextPage, replace: false }));
+			dispatch(getResults({ year: showResultsYear, month: showResultsMonth + 1, page: nextPage, replace: false }));
 			setPage(nextPage);
 		}
 	}
