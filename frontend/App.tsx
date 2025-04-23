ed97c66d25bdf3c61b576e79b6d408c6f83e7a2f
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-04-23 17:48:54 +0300

    fix: run android in expo

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,4 +1,5 @@
 import "react-native-gesture-handler";
+import "react-native-reanimated"
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./src/redux/store";
