5b66e4fd7aef2e0d2fdd6e34d452265fd1a8ccb1
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-05-31 22:14:39 +0300

    feat: create networkSlice to keep status in store

---
+++ frontend/src/redux/networkReducer/networkSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface NetworkState {
+    isConnected: boolean;
+}
+
+const initialState: NetworkState = {
+    isConnected: true
+}
+
+const networkSlice = createSlice({
+    name: "network",
+    initialState,
+    reducers: {
+        setIsConnected(state, action: PayloadAction<boolean>) {
+            state.isConnected = action.payload;
+        }
+    }
+});
+
+export const { setIsConnected } = networkSlice.actions;
+export const networkReducer = networkSlice.reducer;
