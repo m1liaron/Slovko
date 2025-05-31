46ec73440f0e85a47e5489983b88469164b13842
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-05-31 22:21:23 +0300

    feat: add offlineQueue and netWork reducers to stored offline data

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -4,6 +4,8 @@ import { groupReducers } from "./groupReducer/groupSlice";
 import { resultReducers } from "./resultReducer/resultSlice";
 import { sharedGroupReducers } from "./sharedGroupReducer/sharedGroupSlice";
 import { userReducers } from "./userReducer/userSlice";
+import { networkReducer } from "./networkReducer/networkSlice";
+import { offlineQueueReducer } from "./offlineQueueReducer/offlineQueueSlice";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 
@@ -13,12 +15,14 @@ const rootReducer = combineReducers({
 	sharedGroups: sharedGroupReducers,
 	cards: cardReducers,
 	results: resultReducers,
+	network: networkReducer,
+	offlineQueue: offlineQueueReducer
 });
 
 const persisConfig = {
 	key: "root",
 	storage: AsyncStorage,
-	whitelist: ["user", "groups", "cards", "results"]
+	whitelist: ["user", "groups", "cards", "results", "offlineQueue", "network"]
 };
 
 const persistedReducer = persistReducer(persisConfig, rootReducer);
