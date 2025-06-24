28b6fa99dcd46953d4bb50b7f1d63263ec1b3a67
Vlad <machinker34@gmail.com>
2025-06-24 22:59:01 +0300

    feat: change API_URL to https

--- frontend/src/common/enums/constants/server-api.ts
+++ frontend/src/common/enums/constants/server-api.ts
@@ -5,6 +5,6 @@ const isEmulator =
 	Platform.constants.Release?.toLowerCase().includes("emu");
 const SERVER_API_URL: string = isEmulator
 	? process.env.API_URL
-	: "http://localhost:3000";
+	: "https://localhost:3000";
 
 export { SERVER_API_URL };
