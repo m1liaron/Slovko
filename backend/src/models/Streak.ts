3c2e82f7596568cd02d4b6e11972184eca50c662
Vlad <machinker34@gmail.com>
2025-07-15 23:12:18 +0300

    feat: rewrite Streak model on Class

--- backend/src/models/Streak.ts
+++ backend/src/models/Streak.ts
@@ -2,30 +2,50 @@ import { sequelize } from "../db/sequelize";
 import { DataTypes } from "sequelize";
 import { v4 as uuidv4 } from "uuid";
 import { User } from "./User";
+import { BaseAttributes, BaseCreationAttributes, CustomModal } from "./CustomModel";
 
-const Streak = sequelize.define("Streak", {
-	id: {
-		type: DataTypes.UUID,
-		defaultValue: uuidv4,
-		primaryKey: true,
-	},
-	date: {
-		type: DataTypes.DATE,
-		allowNull: false,
-	},
-	frozen: {
-		type: DataTypes.BOOLEAN,
-		allowNull: false,
-		defaultValue: false,
-	},
-	userId: {
-		type: DataTypes.UUID,
-		allowNull: false,
-		references: {
-			model: User,
-			key: "id",
+interface StreakAttributes extends BaseAttributes {
+	date: Date;
+	frozen: number;
+	userId: string;
+}
+
+interface StreakCreationAttributes extends BaseCreationAttributes<StreakAttributes> { }
+
+class Streak extends CustomModal<StreakAttributes, StreakCreationAttributes> implements StreakAttributes {
+	public date!: Date;
+	public frozen!: number;
+	public userId!: string;
+}
+
+Streak.init(
+	{
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: uuidv4,
+			primaryKey: true,
 		},
+		date: {
+			type: DataTypes.DATE,
+			allowNull: false,
+		},
+		frozen: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false,
+		},
+		userId: {
+			type: DataTypes.UUID,
+			allowNull: false,
+			references: {
+				model: User,
+				key: "id",
+			}
+		}
 	},
-});
+	{
+		sequelize
+	}
+)
 
 export { Streak }
