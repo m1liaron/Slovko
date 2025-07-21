88c25929a2b122ed666828ff40afc1adf5612c11
Vlad <machinker34@gmail.com>
2025-07-21 16:59:25 +0300

    feat: wrote test for copy shared group

--- backend/src/tests/sharedGroup.test.ts
+++ backend/src/tests/sharedGroup.test.ts
@@ -1,17 +1,19 @@
 import { testData, authRequest } from "./testSetup.js";
 
 let sharedGroupId = "";
+let sharedGroupTitle = "";
 
 describe("SHARED_GROUP_ROUTES", async () => {
   it("POST_SHARED_GROUP", async () => {
     const res = await authRequest("post", "/sharedGroups", {
-      title: testData.testGroup.title,
+      title: "New shared group",
       groupId: testData.testGroup.id,
     });
 
     expect(res.statusCode).toBe(200);
-    expect(res.body.title).toBe(testData.testGroup.title);
+    expect(res.body.title).toBe("New shared group");
     sharedGroupId = res.body.id;
+    sharedGroupTitle = res.body.title;
   });
 
   it("GET_SHARED_GROUPS", async () => {
@@ -26,6 +28,13 @@ describe("SHARED_GROUP_ROUTES", async () => {
 
     expect(res.statusCode).toBe(200);
     expect(res.body.id).toBe(sharedGroupId);
+  });
+
+  it("COPY_SHARED_GROUP", async () => {
+    const res = await authRequest("post", `/sharedGroups/${sharedGroupId}`);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.title).toBe(sharedGroupTitle);
   });
 
   it("DELETE_SHARED_GROUP by id", async () => {
