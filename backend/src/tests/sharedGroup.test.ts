6ac5735a18f81c04afcdc23d7931a4c828c39d19
Vlad <machinker34@gmail.com>
2025-07-21 15:49:55 +0300

    feat: wrote test for shared group: post, get, delete

--- backend/src/tests/sharedGroup.test.ts
+++ backend/src/tests/sharedGroup.test.ts
@@ -1,5 +1,7 @@
 import { testData, authRequest } from "./testSetup.js";
 
+let sharedGroupId = "";
+
 describe("SHARED_GROUP_ROUTES", async () => {
   it("POST_SHARED_GROUP", async () => {
     const res = await authRequest("post", "/sharedGroups", {
@@ -9,12 +11,27 @@ describe("SHARED_GROUP_ROUTES", async () => {
 
     expect(res.statusCode).toBe(200);
     expect(res.body.title).toBe(testData.testGroup.title);
+    sharedGroupId = res.body.id;
   });
 
-  it("GET_SHARED_GROUP", async () => {
+  it("GET_SHARED_GROUPS", async () => {
     const res = await authRequest("get", "/sharedGroups");
 
     expect(res.statusCode).toBe(200);
     expect(res.body.sharedGroups.length).toBe(1);
+  });
+
+  it("GET_SHARED_GROUP by id", async () => {
+    const res = await authRequest("get", `/sharedGroups/${sharedGroupId}`);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.id).toBe(sharedGroupId);
+  });
+
+  it("DELETE_SHARED_GROUP by id", async () => {
+    const res = await authRequest("delete", `/sharedGroups/${sharedGroupId}`);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBe(sharedGroupId);
   });
 });
