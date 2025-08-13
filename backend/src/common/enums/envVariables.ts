0bff65841293d6ccc5f1c6aaf980e8ef05247b0f
Vlad <machinker34@gmail.com>
2025-08-13 14:33:05 +0300

    fix: docker compose build

--- backend/src/common/enums/envVariables.ts
+++ backend/src/common/enums/envVariables.ts
@@ -2,15 +2,15 @@ import { config } from "dotenv";
 config();
 
 const EnvVariables = {
-    PORT: process.env.PORT as string || 3000,
-    DATABASE_USER_NAME: process.env.DATABASE_USER_NAME  as string || "postgres",
-    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-    DATABASE_NAME: process.env.DATABASE_NAME  as string || "slovko",
-    DATABASE_HOST: process.env.DB_HOST  as string || "localhost",
-    DB_PORT: process.env.DB_PORT  as string || "5434",
-    JWT_SECRET: process.env.JWT_SECRET  as string,
-    JWT_LIFETIME: process.env.JWT_LIFETIME  as string || "30d",
-    UNSPLASH_KEY: process.env.UNSPLASH_KEY  as string
-} as const
+  PORT: (process.env.PORT as string) || 3000,
+  DATABASE_USER_NAME: (process.env.DATABASE_USER_NAME as string) || "postgres",
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+  DATABASE_NAME: (process.env.DATABASE_NAME as string) || "slovko",
+  DATABASE_HOST: (process.env.DATABASE_HOST as string) || "localhost",
+  DB_PORT: (process.env.DB_PORT as string) || "5434",
+  JWT_SECRET: process.env.JWT_SECRET as string,
+  JWT_LIFETIME: (process.env.JWT_LIFETIME as string) || "30d",
+  UNSPLASH_KEY: process.env.UNSPLASH_KEY as string,
+} as const;
 
 export { EnvVariables };
