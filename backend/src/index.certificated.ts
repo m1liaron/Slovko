6c2947167916fa48ad82c67f65b6e4968b7129ca
Vlad <machinker34@gmail.com>
2025-11-13 22:19:33 +0200

    feat: created file for certificated backend and wrote reame.md

--- backend/src/index.certificated.ts
+++ backend/src/index.certificated.ts
@@ -1,5 +1,8 @@
 import express, { Application } from "express";
 import cors from "cors";
+import helmet from "helmet";
+import fs from "fs";
+import https from "https";
 
 import { connectDB, sequelize } from "./db/sequelize.js";
 import {
@@ -22,6 +25,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use(initializeLogger);
+app.use(helmet());
 
 app.use("/users", userRoute);
 app.use("/languages", authMiddleware, languageRoute);
@@ -33,6 +37,11 @@ app.use("/sections", authMiddleware, sectionRoute);
 
 const port = EnvVariables.PORT || 3000;
 
+const SSL_KEY_PATH = process.env.SSL_KEY_PATH || "../certs/key.pem";
+const SSL_CERT_PATH = process.env.SSL_CERT_PATH || "../certs/key.pem";
+const SSL_CA_PATH = process.env.SSL_CA_PATH || undefined; // optional CA chain
+const SSL_PASSPHRASE = process.env.SSL_PASSPHRASE || undefined;
+
 const start = async () => {
   try {
     validateEnvVariables();
@@ -41,8 +50,24 @@ const start = async () => {
     await sequelize.sync({ alter: true });
     await ensureLanguages();
 
-    app.listen(port, () => {
-      console.log(`Server running on port http://127.0.0.1:${port}`);
+    const key = fs.readFileSync(SSL_KEY_PATH, "utf8");
+    const cert = fs.readFileSync(SSL_CERT_PATH, "utf8");
+    const ca = SSL_CA_PATH ? fs.readFileSync(SSL_CA_PATH, "utf8") : undefined;
+
+    const sslOptions: https.ServerOptions = {
+      key,
+      cert,
+      ca,
+      passphrase: SSL_PASSPHRASE,
+    };
+
+    const httpsServer = https.createServer(sslOptions, app);
+
+    httpsServer.listen(port, () => {
+      console.log(`HTTPS server running on port https://localhost:${port}`);
+      console.log(
+        `Protected HTTPS server running on port https://127.0.0.1:${port}`,
+      );
     });
   } catch (error) {
     console.error("Error starting server: ", error);
